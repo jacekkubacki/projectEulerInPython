002a84a574f210fa108761a0c555bb12774345d1
Jacek Kubacki <jacekkubacki@users.noreply.github.com>
2014-07-30 17:11:55 +0100

    Solution to Problem 20

---
+++ euler020.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+
+'''
+Problem 20
+
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+'''
+
+import math
+print ("Result:", sum(map(int, list(str(math.factorial(100))))))
