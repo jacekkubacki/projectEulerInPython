c9d2da678e2b43f9d99a042d8447a609398c97db
Jacek Kubacki <jacekkubacki@users.noreply.github.com>
2014-07-30 17:20:40 +0100

    Solution to Problem 22

---
+++ euler022.py
@@ -0,0 +1,49 @@
+#!/usr/local/bin/python3
+'''
+Names scores
+Problem 22
+
+Using names.txt (right click and 'Save Link/Target As...'),
+a 46K text file containing over five-thousand first names,
+begin by sorting it into alphabetical order.
+Then working out the alphabetical value for each name,
+multiply this value by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order,
+COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
+So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file?
+'''
+
+import string
+
+# to use it in map()
+# more readable than map(lambda s: s[1:-1], ...)
+def dequote(s):
+    return s[1:-1]
+    
+def namescore(s):
+    alphabet = ['']
+    alphabet += list (string.ascii_uppercase)
+    name = list (s.upper())
+
+    result = 0
+    for letter in name: 
+        result += alphabet.index(letter)
+
+    return result
+
+
+f = open ('names.txt', 'r')
+data=f.read()
+f.close()
+
+listOfNames = list(map(dequote, list(str(data).split(','))))
+listOfNames.sort()
+
+result = 0
+for i in range (0, len(listOfNames)):
+    result += (i + 1) * namescore(listOfNames[i])
+
+print ("Result:", result)
