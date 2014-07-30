eb2238072a702e23655c6f982ca0e2d9e5aab383
Jacek Kubacki <jacekkubacki@users.noreply.github.com>
2014-07-30 09:41:14 +0100

    comments updated

--- euler004.py
+++ euler004.py
@@ -13,9 +13,8 @@ palindrome = 0
 limit = 100 # the smallest 3-digit number
 
 # starting with the big numbers
-# x will be always >= y
 for x in range (999, limit, -1):
-    # y <= x
+    # x will be always >= y
     for y in range (x, limit, -1):
         p = x * y
         s = str (p)
@@ -26,11 +25,10 @@ for x in range (999, limit, -1):
             if palindrome < p:
                 palindrome = p
                 # there is no need to check numbers lower than y
+		# as the product is guaranteed to be smaller than x*y
                 limit = y 
                 # the palindrome cannot be biger for a given x
                 break
                 
 print ("Result:", palindrome)
-
-
     
