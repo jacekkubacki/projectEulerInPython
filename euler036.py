bb55b6f01f56a6caff3f1cd9a10e97f9f4aeae1e
Jacek Kubacki <jacekkubacki@users.noreply.github.com>
2014-08-28 17:26:43 +0100

    One loop only, [::-1] to reverse string

--- euler036.py
+++ euler036.py
@@ -4,34 +4,19 @@ Double-base palindromes
 Problem 36
 
 The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
-
 Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
-
 (Please note that the palindromic number, in either base, may not include leading zeros.)
 '''
 
-def dec2bin (i):
-    return bin(i)[2:]
+result = 0
 
-palindromes=[]
-# Build the list of decimal palindromes.
 # We skip even numbers as they end with '0' in base 2
 # and as a consequence are not palindromic
 for p in range (1, 1000000, 2):
-    l = list (str(p))
-    r = list (str(p))
-    r.reverse()
-    if l == r:
-        palindromes.append(p)
+    # Is it a decimal palindrome?
+    if str(p) == str(p)[::-1]:
+        # Is it a binary palindrome?
+        if bin(p)[2:] == bin(p)[:1:-1]:
+            result += p
 
-doubleBasedPalindromes = []
-# Parse the list of decimal palindromes and build the list of binary palindromes 
-# The numbers in the list will be both decimal and binary palindromes
-for p in palindromes:
-    l = list (dec2bin(p))
-    r = list (l)
-    r.reverse()
-    if l == r:
-        doubleBasedPalindromes.append(p)
-
-print ("Result:", sum(doubleBasedPalindromes))
+print ("Result:", result)
