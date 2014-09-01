607a6f9fa5e36b4a1907e97bfe03a62a6701ace6
Jacek Kubacki <jacekkubacki@users.noreply.github.com>
2014-09-02 00:03:00 +0100

    Solution to Problem 15

---
+++ euler015.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3
+'''
+Problem 15
+
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20×20 grid?
+'''
+
+'''
+Solution:
+Every route will contain 20 'downs' and 20 'rights'.
+There 40! possible ways of arranging 40 elements, but we can't distinguish one 'down' from the other.
+There 20! possible ways of arranging 'downs' that will give us the same route, same for 'rights'.
+So the answer is 40!/(20!*20!).
+'''
+
+from math import factorial
+print ("Result:", factorial(40)//pow(factorial(20),2))
+                                      
