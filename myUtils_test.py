804a3a020beb22058870fdb7578df4bd62054da0
Jacek Kubacki <jacekkubacki@users.noreply.github.com>
2014-09-02 14:03:18 +0100

    Refactored and new tests added

--- myUtils_test.py
+++ myUtils_test.py
@@ -7,24 +7,28 @@ To run the tests:
 
 import subprocess
 
-def test_primeFactorization():
-    solution = {'euler005.py' : 'Result: 232792560',\
-                'euler012.py' : 'Result: 76576500'}
-
-    for key, value in solution.items():
+def runSolutions (s):
+    for key, value in s.items():
         stdout = str(subprocess.check_output('python3 ' + key, shell = True, universal_newlines=True))
         assert value in stdout
-
     
-    stdout = str(subprocess.check_output('python3 euler005.py', shell=True, universal_newlines=True))
-    assert 'Result: 232792560' in stdout
+
+def test_primeFactorization():
+    solutions = {'euler005.py' : 'Result: 232792560',\
+                 'euler012.py' : 'Result: 76576500'}
+    runSolutions(solutions)
 
 def test_listOfPrimes():
-    solution = {'euler010.py' : 'Result: 142913828922',\
-                'euler035.py' : 'Result: 55',\
-                'euler049.py' : 'Result: 296962999629',\
-                'euler050.py' : 'Result: 997651'}
+    solutions = {'euler010.py' : 'Result: 142913828922',\
+                 'euler035.py' : 'Result: 55',\
+                 'euler049.py' : 'Result: 296962999629',\
+                 'euler050.py' : 'Result: 997651'}
+    runSolutions(solutions)
+    
+def test_dequote():
+    solutions = {'euler042.py' : 'Result: 162'}
+    runSolutions(solutions)
 
-    for key, value in solution.items():
-        stdout = str(subprocess.check_output('python3 ' + key, shell = True, universal_newlines=True))
-        assert value in stdout
+def test_wordValue():
+    solutions = {'euler042.py' : 'Result: 162'}
+    runSolutions(solutions)
