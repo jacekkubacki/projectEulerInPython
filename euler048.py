8c0a554080b064a13f89c6eb63774a48803a7387
Jacek Kubacki <jacekkubacki@users.noreply.github.com>
2014-09-03 01:04:10 +0100

    Solution to Problem 48

---
+++ euler048.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+'''
+Self powers
+Problem 48
+
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+'''
+
+print ("Result:", str(sum([pow(i,i) for i in range (1,1000+1)]))[-10:])
