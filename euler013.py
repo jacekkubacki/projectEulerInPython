a13af9214e2e3b350ecf629ef749c566fa9a9765
Jacek Kubacki <jacekkubacki@users.noreply.github.com>
2014-09-07 20:11:01 +0100

    Title added

--- euler013.py
+++ euler013.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python3
-
 '''
+Large sum
 Problem 13
+
 Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 
 37107287533902102798797998220837590246510135740250
