ad812b974ee171d151fb90a11e25972b225b7ce2
Jacek Kubacki <jacekkubacki@users.noreply.github.com>
2015-08-20 16:50:22 +0100

    Cosmetic change

--- euler081.py
+++ euler081.py
@@ -2,6 +2,7 @@
 
 # Path sum: two ways
 # Problem 81
+#
 # In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right, by only moving to the right and down, is indicated in bold red and is equal to 2427.
 #
 # 131 673 234 103  18
