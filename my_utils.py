9271bd8e607a1ad6551ed2bf6419314a1fcfcdfd
Jacek Kubacki <jacekkubacki@users.noreply.github.com>
2017-12-12 01:24:39 +0000

    my_utils.py more refactoring

--- my_utils.py
+++ my_utils.py
@@ -1,3 +1,7 @@
+"""
+This module contains functions used by two or more solutions.
+There is no error checking to speed up the execution.
+"""
 import itertools
 import math
 import re
@@ -5,17 +9,17 @@ import string
 
 
 def arithmetic_sum(a1, diff, n):
-    """Returns sum of arithmetic progression"""
+    """Return sum of arithmetic progression"""
     return (2 * a1 + (n - 1) * diff) * (n / 2.0)
 
 
 def dequote(s):
-    """Returns the string with leading and trailing double quote character removed"""
+    """Return the string with leading and trailing double quote character removed"""
     return re.sub(r'^"|"$', '', s)
 
 
-def is_prime (number):
-    """Returns True is number is prime, False otherwise"""
+def is_prime(number):
+    """Return True is number is prime, False otherwise"""
     # This algorithm checks if the given number can be divided by integers of the form 6k +/- 1
     # see: http://en.wikipedia.org/wiki/Primality_test#Naive_methods
     if number <= 3:
@@ -29,11 +33,11 @@ def is_prime (number):
 
 
 def list_of_divisors(number):
-    """Returns list of numbers less than n which divide evenly into n"""
+    """Return list of numbers less than n which divide evenly into n"""
     if number <= 1:
         return []
 
-    divisors = set([1])
+    divisors = {1}
     factors = list_of_prime_factors(number)
 
     for r in range(1, len(factors)):
@@ -46,10 +50,10 @@ def list_of_divisors(number):
     return list(divisors)
 
 
-def list_of_primes (n):
-    """Returns ordered list of primes in range(2,n)"""
+def list_of_primes(n):
+    """Return ordered list of primes in range(2,n)"""
     # linear sieve algorithm: http://edu.i-lo.tarnow.pl/inf/alg/001_search/0012.php
-    primes = [True for i in range(0, n)]
+    primes = [True] * n
     p = 2
     while p * p < n:
         q = p
@@ -60,21 +64,23 @@ def list_of_primes (n):
                 x *= p
             while True:
                 q += 1
-                if primes[q]: break
+                if primes[q]:
+                    break
         while True:
             p += 1
-            if primes[p]: break
+            if primes[p]:
+                break
 
     return [i for i in range(2, n) if primes[i]]
 
 
 def list_of_prime_factors(n):
-    """Returns UNSORTED list of prime factors of n"""
+    """Return UNSORTED list of prime factors of n"""
     # all the prime factors that are < n are also <= sqrt(n)
-    limit = int (math.sqrt(n))
+    limit = int(math.sqrt(n))
 
     # let's start with 2 to break down even numbers
-    for i in range (2, limit + 1):
+    for i in range(2, limit + 1):
         if n % i == 0:
             # factors found, try to factorize them
             return list_of_prime_factors(i) + list_of_prime_factors(n // i)
@@ -91,9 +97,12 @@ def sum_of_digits(number):
     return s
 
 
+# global variable
 alphabet = [''] + list(string.ascii_uppercase)
+
+
 def word_value(s):
-    """Converts each letter in a word to a number corresponding to its alphabetical position and adds these values"""
+    """Convert each letter in a word to a number corresponding to its alphabetical position and add these values"""
     word = list(s.upper())
     result = 0
     for letter in word:
